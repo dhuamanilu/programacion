c83dfaa38c0a2d0c32784b43b20df012db32adbd
dhuamanilu <87215556+dhuamanilu@users.noreply.github.com>
2024-01-23 17:13:08 -0500

    Update 2205.cpp

--- CSES/2205.cpp
+++ CSES/2205.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -18,78 +20,40 @@ typedef priority_queue<ll> pq;
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define ins insert
-#ifdef LOCAL
-	#define n_l '\n'
-	#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
-	template <typename T, size_t N> int SIZE(const T (&t)[N]){ return N; } template<typename T> int SIZE(const T &t){ return t.size(); } string to_string(const string s, int x1=0, int x2=1e9){ return '"' + ((x1 < s.size()) ? s.substr(x1, x2-x1+1) : "") + '"'; } string to_string(const char* s) { return to_string((string) s); } string to_string(const bool b) { return (b ? "true" : "false"); } string to_string(const char c){ return string({c}); } template<size_t N> string to_string(const bitset<N> &b, int x1=0, int x2=1e9){ string t = ""; for(int __iii__ = min(x1,SIZE(b)),  __jjj__ = min(x2, SIZE(b)-1); __iii__ <= __jjj__; ++__iii__){ t += b[__iii__] + '0'; } return '"' + t + '"'; } template <typename A, typename... C> string to_string(const A (&v), int x1=0, int x2=1e9, C... coords); int l_v_l_v_l = 0, t_a_b_s = 0; template <typename A, typename B> string to_string(const pair<A, B> &p) { l_v_l_v_l++; string res = "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; l_v_l_v_l--; return res; } template <typename A, typename... C> string to_string(const A (&v), int x1, int x2, C... coords) { int rnk = rank<A>::value; string tab(t_a_b_s, ' '); string res = ""; bool first = true; if(l_v_l_v_l == 0) res += n_l; res += tab + "["; x1 = min(x1, SIZE(v)), x2 = min(x2, SIZE(v)); auto l = begin(v); advance(l, x1); auto r = l; advance(r, (x2-x1) + (x2 < SIZE(v))); for (auto e = l; e != r; e = next(e)) { if (!first) { res += ", "; } first = false; l_v_l_v_l++; if(e != l){ if(rnk > 1) { res += n_l; t_a_b_s = l_v_l_v_l; }; } else{ t_a_b_s = 0; } res += to_string(*e, coords...); l_v_l_v_l--; } res += "]"; if(l_v_l_v_l == 0) res += n_l; return res; } void dbgm(){;} template<typename Heads, typename... Tails> void dbgm(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgm(T...); }
-	#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgm(__VA_ARGS__); cout << endl
-#else
-	#define dbg(...)
-	#define dbgm(...)
-#endif
+#define n_l '\n'
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+template <typename T, size_t N> int SIZE(const T (&t)[N]){ return N; } template<typename T> int SIZE(const T &t){ return t.size(); } string to_string(const string s, int x1=0, int x2=1e9){ return '"' + ((x1 < s.size()) ? s.substr(x1, x2-x1+1) : "") + '"'; } string to_string(const char* s) { return to_string((string) s); } string to_string(const bool b) { return (b ? "true" : "false"); } string to_string(const char c){ return string({c}); } template<size_t N> string to_string(const bitset<N> &b, int x1=0, int x2=1e9){ string t = ""; for(int __iii__ = min(x1,SIZE(b)),  __jjj__ = min(x2, SIZE(b)-1); __iii__ <= __jjj__; ++__iii__){ t += b[__iii__] + '0'; } return '"' + t + '"'; } template <typename A, typename... C> string to_string(const A (&v), int x1=0, int x2=1e9, C... coords); int l_v_l_v_l = 0, t_a_b_s = 0; template <typename A, typename B> string to_string(const pair<A, B> &p) { l_v_l_v_l++; string res = "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; l_v_l_v_l--; return res; } template <typename A, typename... C> string to_string(const A (&v), int x1, int x2, C... coords) { int rnk = rank<A>::value; string tab(t_a_b_s, ' '); string res = ""; bool first = true; if(l_v_l_v_l == 0) res += n_l; res += tab + "["; x1 = min(x1, SIZE(v)), x2 = min(x2, SIZE(v)); auto l = begin(v); advance(l, x1); auto r = l; advance(r, (x2-x1) + (x2 < SIZE(v))); for (auto e = l; e != r; e = next(e)) { if (!first) { res += ", "; } first = false; l_v_l_v_l++; if(e != l){ if(rnk > 1) { res += n_l; t_a_b_s = l_v_l_v_l; }; } else{ t_a_b_s = 0; } res += to_string(*e, coords...); l_v_l_v_l--; } res += "]"; if(l_v_l_v_l == 0) res += n_l; return res; } void dbgm(){;} template<typename Heads, typename... Tails> void dbgm(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgm(T...); }
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgm(__VA_ARGS__); cout << endl
 const int MOD = 1000000007;
 const char nl = '\n';
 const int MX = 100001;
 const int N=1000+3;
-ll dist(ll a,ll b){
-	ll cont=0;
-	FOR(i,0,19){
-		ll act=(1ll<<i);
-		if((act&a)!=(act&b)){
-			cont++;
-		}
-	}
-	return cont;
-	
-}
+
 void solve(){
     ll n;
     cin>>n;
-    vll a;
-    FOR(i,0,n){
-    	
+    vector<string> ans={"1","0"};
+    ll tam=(1ll<<n);
+    while(ans.size()<tam){
+        vector<string> copia=ans;
+        reverse(all(copia));
+        ans.insert(ans.end(),copia.begin(),copia.end());
+        FOR(i,0,ans.size()/2){
+            ans[i]+="1";
+        }
+        FOR(i,ans.size()/2,ans.size()){
+            ans[i]+="0";
+        }
     }
-    /*FOR(i,0,(1ll<<n)){
-        a.pb(i);
+    for(auto&e : ans){
+        cout<<e<<"\n";
     }
-    do{
-    	bool ok=true;
-    	ll cont=0;
-    	vll aux={0,1,3,2};
-    	FOR(i,0,(ll)a.size()-1){
-    		if(a==aux){
-    			dbgm("especial",cont,dist(a[i],a[i+1]));
-    		}
-    		if(dist(a[i],a[i+1])==1){
-    			cont++;
-    		}
-    	}
-    	if(a==aux){
-    		dbgm("especial",cont);
-    	}
-    	ok&=(cont==(a.size()-1));
-    	if(ok){
-    		if(n==3){
-    			if(a.front()==7
-    			&& a[1]==3){
-    				dbg(a);
-    			}
-    		}
-    		else{
-    			dbg(a);
-    		}
-    		
-    	}
-    }while(next_permutation(all(a)));*/
-    
-    
-    
+    //dbg(ans);
 }
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int t=1;
-    cin>>t;
     while(t--){
         solve();
     }
