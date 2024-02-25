2113a7ddb665ce095b96b7acb54b69c1c1a21f0c
Diego <dhuamanilu@unsa.edu.pe>
2024-02-25 13:59:34 -0500

    upsolving 1400 problems

--- 1759/1759D.cpp
+++ 1759/1759D.cpp
@@ -15,6 +15,15 @@ typedef vector<ll> vll;
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define ins insert
+#ifdef LOCAL
+	#define n_l '\n'
+	#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+	template <typename T, size_t N> int SIZE(const T (&t)[N]){ return N; } template<typename T> int SIZE(const T &t){ return t.size(); } string to_string(const string s, int x1=0, int x2=1e9){ return '"' + ((x1 < s.size()) ? s.substr(x1, x2-x1+1) : "") + '"'; } string to_string(const char* s) { return to_string((string) s); } string to_string(const bool b) { return (b ? "true" : "false"); } string to_string(const char c){ return string({c}); } template<size_t N> string to_string(const bitset<N> &b, int x1=0, int x2=1e9){ string t = ""; for(int __iii__ = min(x1,SIZE(b)),  __jjj__ = min(x2, SIZE(b)-1); __iii__ <= __jjj__; ++__iii__){ t += b[__iii__] + '0'; } return '"' + t + '"'; } template <typename A, typename... C> string to_string(const A (&v), int x1=0, int x2=1e9, C... coords); int l_v_l_v_l = 0, t_a_b_s = 0; template <typename A, typename B> string to_string(const pair<A, B> &p) { l_v_l_v_l++; string res = "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; l_v_l_v_l--; return res; } template <typename A, typename... C> string to_string(const A (&v), int x1, int x2, C... coords) { int rnk = rank<A>::value; string tab(t_a_b_s, ' '); string res = ""; bool first = true; if(l_v_l_v_l == 0) res += n_l; res += tab + "["; x1 = min(x1, SIZE(v)), x2 = min(x2, SIZE(v)); auto l = begin(v); advance(l, x1); auto r = l; advance(r, (x2-x1) + (x2 < SIZE(v))); for (auto e = l; e != r; e = next(e)) { if (!first) { res += ", "; } first = false; l_v_l_v_l++; if(e != l){ if(rnk > 1) { res += n_l; t_a_b_s = l_v_l_v_l; }; } else{ t_a_b_s = 0; } res += to_string(*e, coords...); l_v_l_v_l--; } res += "]"; if(l_v_l_v_l == 0) res += n_l; return res; } void dbgm(){;} template<typename Heads, typename... Tails> void dbgm(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgm(T...); }
+	#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgm(__VA_ARGS__); cout << endl
+#else
+	#define dbg(...)
+	#define dbgm(...)
+#endif
 const int MOD = 1000000007;
 const char nl = '\n';
 const int MX = 100001;
@@ -23,6 +32,7 @@ const int N=1000+3;
 void solve(){
     ll n,m;
     cin>>n>>m;
+    ll n2=n;
     ll cont2=0,cont5=0;
     while(n%2==0){
         cont2++;
@@ -32,76 +42,21 @@ void solve(){
         cont5++;
         n/=5;
     }
-    ll mult=1;
-    if(cont2==cont5){
-        bool ok=false;
-        while(mult<=m){
-            mult*=10;
-            ok=true;
-        }
-        if(ok) mult/=10;
-        m/=mult;
-        mult*=(m/10)*10;
-        cout<<n*mult<<"\n";
-    }
-    else if(cont2<cont5){
-        if(pow(2,cont5-cont2) <= m ){
-            mult*=cont5-cont2;
-            bool ok=false;
-            while(mult<=m){
-                mult*=10;
-                ok=true;
-            }
-            if(ok) mult/=10;
-            cout<<n*mult<<"\n";
-        }
-        else{
-            ll tot=1;
-            while(tot<=m){
-                tot*=2;
-            }
-            tot/=2;
-
-            mult*=tot;
-            bool ok=false;
-            while(mult<=m){
-                mult*=10;
-                ok=true;
-            }
-            if(ok) mult/=10;
-            cout<<n*mult<<"\n";
-        }
-    }
-    else{
-        if(pow(2,cont2-cont5) <= m ){
-            bool ok=false;
-            mult*=cont2-cont5;
-            while(mult<=m){
-                mult*=10;
-                ok=true;
-            }
-            if(ok) mult/=10;
-            cout<<n*mult<<"\n";
-        }
-        else{
-            ll tot=1;
-            while(tot<=m){
-                tot*=5;
-            }
-            tot/=5;
-            mult*=tot;
-
-            bool ok=false;
-            while(mult<=m){
-                mult*=10;
-                ok=true;
-            }
-            if(ok) mult/=10;
-            cout<<n*mult<<"\n";
-
-        }
-    }
-
+    ll k=1;   
+	while(k*2<=m && cont2<cont5){
+		k*=2;
+		cont2++;
+	}
+	while(k*5<=m && cont2>cont5){
+		k*=5;
+		cont5++;
+	}
+	while(k*10<=m){
+		k*=10;
+	}
+	ll ult=m/k;
+	cout<<n2*k*ult<<"\n";
+	//dbgm(n2,k,m);
 }
 int main(){
     ios_base::sync_with_stdio(0);
--- 1759/1759E.cpp
+++ 1759/1759E.cpp
@@ -19,16 +19,6 @@ const int MOD = 1000000007;
 const char nl = '\n';
 const int MX = 100001;
 const int N=1000+3;
-long long binpow(long long a, long long b) {
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
-    }
-    return res;
-}
 void solve(){
     ll n,h;
     cin>>n>>h;
@@ -37,38 +27,22 @@ void solve(){
         cin>>a[i];
     }
     sort(all(a));
-    ll green=2,blue=1,ans=0;
-    FOR(i,0,n){
-    	if(h>a[i]){
-    		h+=a[i]/2;
-    		ans++;
-    		continue;
+    ll ans=0;
+    vector<vll> orders={{3,2,2},{2,3,2},
+    {2,2,3}};
+    for(auto & order : orders){
+    	ll id=0,nh=h,res=0;
+    	for(ll j=0;j<n;){
+    		if(a[j]<nh){
+    			res++;
+    			nh+=a[j]/2;
+    			j++;
+    			continue;
+    		}
+    		if(id==3) break;
+    		nh*=order[id++];
     	}
-    	ll ind=-1;
-    	pair<ll,ll> response={0,0};
-        FOR1(k,0,1){
-        	FOR1(j,0,2){
-        		if(green>=j && blue>= k){
-        			ll newH=h*binpow(2,j)*binpow(3,k);
-        			ll newInd=lower_bound(all(a),newH)-a.begin();
-        			if(newInd>ind){
-        				ind=newInd;
-        				response=mp(j,k);
-        			}
-        			else if(newInd==ind){
-        				response=min(response,mp(j,k));
-        			}
-        		}
-        	}
-        }
-        h*=binpow(2,response.f)*binpow(3,response.se);
-        green-=response.f;
-        blue-=response.se;
-        
-        if(h>a[i]){
-    		h+=a[i]/2;
-    		ans++;
-    	}
+    	ans=max(ans,res);
     }
     cout<<ans<<"\n";
 }
