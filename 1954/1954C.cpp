7c34b61a17a19628de5742b5ffee672b138eb7ce
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-08-11 22:22:56 -0500

    doing contest

---
+++ 1954/1954C.cpp
@@ -0,0 +1,237 @@
+//? #pragma GCC optimize ("Ofast")
+//? #pragma GCC target ("avx,avx2")
+//! #pragma GCC optimize ("trapv")
+//#undef _GLIBCXX_DEBUG //? for Stress Testing
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "../debugICPC.h"
+#define chk(...) if (!(__VA_ARGS__)) cerr << "\033[41m" << "Line(" << __LINE__ << ") -> function(" \
+	 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\033[0m" << "\n", exit(0);
+
+#define MACRO(code) do {code} while (false)
+#define RAYA MACRO(cerr << "\033[101m" << "================================" << "\033[0m" << endl;)
+#else
+#define dbg(...)
+
+#define chk(...)
+#define RAYA
+#endif
+
+const auto beg_time = std::chrono::high_resolution_clock::now();
+// https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
+double time_elapsed() {
+	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
+	                                beg_time)
+	    .count();
+}
+
+// building blocks
+using ll  = long long;
+using db  = long double; // or double, if TL is tight
+using str = string;      // yay python!
+//? priority_queue for minimum
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+using ull  = unsigned long long;
+//? using i64  = long long;
+//? using u64  = uint64_t;
+//? using i128 = __int128;
+//? using u128 = __uint128_t;
+//? using f128 = __float128;
+// pairs
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template <class T
+#define tcTU tcT, class U
+//! ^ lol this makes everything look weird but I'll try it
+tcT > using V = vector<T>;
+tcT, size_t SZ > using AR = array<T, SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define ts to_string
+
+
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+
+
+const int MOD = 1e9+7;
+const ll BIG = 1e18;  //? not too close to LLONG_MAX
+const db PI = acos((db)-1);
+mt19937 rng(0); // or mt19937_64
+
+
+
+ll cdiv(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll fdiv(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
+	sort(all(v));
+	v.erase(unique(all(v)), end(v));
+}
+tcTU > void safeErase(T &t, const U &u) {
+	auto it = t.find(u);
+	assert(it != end(t));
+	t.erase(it);
+}
+
+
+
+inline namespace FileIO {
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
+	//? cout << fixed << setprecision(12);
+    //? cerr << fixed << setprecision(12);
+	cin.exceptions(cin.failbit);
+	// throws exception when do smth illegal
+	// ex. try to read letter into int
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");  // for old USACO
+}
+}  // namespace FileIO
+
+
+
+//? Custom Helpers
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//? /Custom Helpers
+
+ll brute(str s1,str s2,ll idx){
+    ll n=s1.size();
+    if(idx==n){
+        return stoll(s1)*stoll(s2);
+    }
+    else{
+        //swapear o no swapear
+        ll ans1=brute(s1,s2,idx+1);
+        swap(s1[idx],s2[idx]);
+        ll ans2=brute(s1,s2,idx+1);
+        return max(ans1,ans2);
+    }
+}
+void solve(str s1,str s2) {
+    ll n=s1.size(),idx=0;
+    FOR(i,0,n){
+        if(s1[i]==s2[i]) continue;
+        char maxi=max(s1[i],s2[i]),mini=min(s1[i],s2[i]);
+        s1[i]=maxi;
+        s2[i]=mini;
+        idx=i;
+        break;
+    }
+    FOR(i,idx+1,n){
+        char maxi=max(s1[i],s2[i]),mini=min(s1[i],s2[i]);
+        s1[i]=mini;
+        s2[i]=maxi;
+    }
+    //return stoll(s1)*stoll(s2);
+    cout<<s1<<"\n"<<s2<<"\n";
+	
+}
+//? /Custom Helpers
+//? Generator
+int rng_int(int L, int R) { assert(L <= R);
+	return uniform_int_distribution<int>(L,R)(rng);  }
+ll rng_ll(ll L, ll R) { assert(L <= R);
+	return uniform_int_distribution<ll>(L,R)(rng);  }
+//? /Generator
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t = 1;
+    cin >> t;
+    /*while(1){
+        ll n=rng_ll(1,6);
+        str s1="",s2="";
+        s1.pb('0'+rng_ll(1,9));
+        s2.pb('0'+rng_ll(1,9));
+        FOR(i,0,n-1){
+            s1.pb('0'+rng_ll(0,9));
+            s2.pb('0'+rng_ll(0,9));
+        }
+        ll ans1=brute(s1,s2,0),ans2=solve(s1,s2);
+        if(ans1 != ans2){
+            dbg("xd",ans1,ans2,s1,s2);
+            assert(false);
+        }
+        else dbg("ok");
+    }*/
+    for(int idx = 0; idx < t; idx++) {
+        RAYA;
+        RAYA;
+        str s1,s2;
+        cin>>s1>>s2;    
+        solve(s1,s2);
+    }
+    RAYA;
+    RAYA;
+
+    #ifdef LOCAL
+        cerr << fixed << setprecision(5);
+        cerr << "\033[42m++++++++++++++++++++\033[0m\n";
+        cerr << "\033[42mtime = " << time_elapsed() << "ms\033[0m\n";
+        cerr << "\033[42m++++++++++++++++++++\033[0m";
+    #endif
+}
+
+
+
+
+
+
+
+
