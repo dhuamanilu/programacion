1020c31ebbf2afdacd1b1fcef152a09190eb09ef
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2024-12-25 19:53:05 -0500

    codechef

--- starters166/PLANTILLA copy.cpp
+++ starters166/PLANTILLA copy.cpp
@@ -1,4 +1,4 @@
-//? #pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("Ofast")
 //? #pragma GCC target ("avx,avx2")
 //! #pragma GCC optimize ("trapv")
 //#undef _GLIBCXX_DEBUG //? for Stress Testing
@@ -154,34 +154,143 @@ int rng_int(int L, int R) { assert(L <= R);
 	return uniform_int_distribution<int>(L,R)(rng);  }
 ll rng_ll(ll L, ll R) { assert(L <= R);
 	return uniform_int_distribution<ll>(L,R)(rng);  }
-//? /Generator
 
-vl solve(vl &a,ll n,ll m) {
-	ll q=a.size();
-	set<ll> pos;
-	pos.insert(m);
-	FOR(i,0,q){
-
+const int TAG=3170;
+bool add_factors(ll x,vi &toAdd){
+    int x2=x;
+    for(int i=2;i*i<=x;i++){
+        if(x2%i==0){
+            int cont=0;
+            while(x2%i==0){
+                cont++;
+                x2/=i;
+            }
+            if(cont>0){
+				toAdd[i]+=cont;
+            }
+        }
+    }
+    if(x2>1){
+		if(x2 < TAG){
+			toAdd[x2]++;
+			return false;
+		}
+		else return true;
+		//toAdd[x2]++;
+    }
+	else return false;
+}
+const int N=10000005;
+vb isPrime(N,1);
+vl primes;
+void init(){
+	FOR(i,2,N){
+		if(isPrime[i]){
+			for(ll j=2*i;j<N;j+=i){
+				isPrime[j]=0;
+			}
+		}
 	}
-	
+	FOR(i,2,N){
+		if(isPrime[i])
+			primes.pb(i);
+	}
+}
+void add_factorsv2(ll x,map<ll,ll> &toAdd){
+    vpl ans;
+    ll x2=x;
+    for(ll i=2;i*i<=x;i++){
+        if(x2%i==0){
+            ll cont=0;
+            while(x2%i==0){
+                cont++;
+                x2/=i;
+            }
+            if(cont>0){
+				toAdd[i]+=cont;
+            }
+        }
+    }
+    if(x2>1){
+		toAdd[x2]++;
+    }
+}
+map<ll,ll> getFactorsLento(ll m){
+	map<ll,ll> descomposicion;
+	FOR(i,1,m+1){
+		add_factorsv2(i,descomposicion);
+	}
+	return descomposicion;
+}
+map<ll,ll> getFactorsRapido(ll m){
+	map<ll,ll> decomp;
+	//TODO : iterar por todos los primos en vez de hasta m
+	FOR(i,0,(ll)primes.size()){
+		ll prime=primes[i];
+		if(prime > m)break;
+		for(int j=prime;j<=m;j*=prime){
+			decomp[prime]+=m/j;
+		}
+	}
+	return decomp;
+}
+vi solve(vi &a,ll m) {
+	ll n=sz(a);
+	vector<vector<int>> factors(n,vi(TAG,0));
+	vb esPrimo(n,0);
+	FOR(i,0,n){
+		bool xd = add_factors(a[i],factors[i]);
+		if(xd) esPrimo[i]=1; 
+	}
+	auto decomp=getFactorsRapido(m);
+	vi ans;
+	FOR(i,0,n){
+		int res=1;
+		auto act=decomp;
+		FOR(j,0,TAG){
+			if(factors[i][j]>0){
+				act[j]+=factors[i][j];
+			}
+		}
+		if(esPrimo[i]){
+			act[a[i]]++;
+		}
+		each(e,act){
+			res*=e.s+1;
+			res%=MOD;
+		}
+		
+		ans.pb(res);
+	}
+	return ans;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int t = 1;
-    cin >> t;
-
+    //cin >> t;
+	init();
+	dbg(primes.size());
+	/*FOR(i,1000,10000){
+		auto m=i;
+		auto x=getFactorsLento(m);
+		auto y=getFactorsRapido(m);
+		if(x!=y){
+			dbg(m,x,y);
+			assert(false);
+		}
+		else dbg("ok");
+	}*/
     for(int idx = 0; idx < t; idx++) {
         RAYA;
         RAYA;
-		ll n,m,q;
-		cin>>n>>m>>q;
-		vl a(q);
+		ll n,m;
+		cin>>n>>m;
+		vi a(n);
 		each(e,a)cin>>e;
-
-        auto xd =solve(a,n,m);
-		eack(e,xd)cout<<e<<" ";
+		auto x = solve(a,m);
+		each(e,x)cout<<e<<" ";
 		cout<<"\n";
     }
     RAYA;
@@ -194,7 +303,6 @@ int main() {
         cerr << "\033[42m++++++++++++++++++++\033[0m";
     #endif
 }
-
 
 
 
--- starters166/versionfinal.cpp
+++ starters166/versionfinal.cpp
@@ -1,7 +1,7 @@
-//? #pragma GCC optimize ("Ofast")
-//? #pragma GCC target ("avx,avx2")
-//! #pragma GCC optimize ("trapv")
-//#undef _GLIBCXX_DEBUG //? for Stress Testing
+#pragma GCC optimize ("Ofast")
+//#pragma GCC target ("avx,avx2")
+#pragma GCC optimize ("trapv")
+#undef _GLIBCXX_DEBUG //? for Stress Testing
 #include <bits/stdc++.h>
 using namespace std;
 #ifdef LOCAL
@@ -17,7 +17,6 @@ using namespace std;
 #define chk(...)
 #define RAYA
 #endif
-
 const auto beg_time = std::chrono::high_resolution_clock::now();
 // https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
 double time_elapsed() {
@@ -25,7 +24,6 @@ double time_elapsed() {
 	                                beg_time)
 	    .count();
 }
-
 // building blocks
 using ll  = long long;
 using db  = long double; // or double, if TL is tight
@@ -45,10 +43,8 @@ using pd = pair<db, db>;
 #define mp make_pair
 #define f first
 #define s second
-
 #define tcT template <class T
 #define tcTU tcT, class U
-//! ^ lol this makes everything look weird but I'll try it
 tcT > using V = vector<T>;
 tcT, size_t SZ > using AR = array<T, SZ>;
 using vi = V<int>;
@@ -59,9 +55,6 @@ using vs = V<str>;
 using vpi = V<pi>;
 using vpl = V<pl>;
 using vpd = V<pd>;
-
-// vectors
-// oops size(x), rbegin(x), rend(x) need C++17
 #define sz(x) int((x).size())
 #define bg(x) begin(x)
 #define all(x) bg(x), end(x)
@@ -74,38 +67,26 @@ using vpd = V<pd>;
 #define ft front()
 #define bk back()
 #define ts to_string
-
-
-
-// loops
 #define FOR(i,a,b) for (int i = (a); i < (b); ++i)
 #define F0R(i,a) FOR(i,0,a)
 #define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
 #define R0F(i,a) ROF(i,0,a)
 #define rep(a) F0R(_,a)
 #define each(a,x) for (auto& a: x)
-
-
-
 const int MOD = 1e9+7;
 const ll BIG = 1e18;  //? not too close to LLONG_MAX
 const db PI = acos((db)-1);
 mt19937 rng(0); // or mt19937_64
-
-
-
 ll cdiv(ll a, ll b) {
 	return a / b + ((a ^ b) > 0 && a % b);
 }  // divide a by b rounded up
 ll fdiv(ll a, ll b) {
 	return a / b - ((a ^ b) < 0 && a % b);
 }  // divide a by b rounded down
-
 tcT> bool ckmin(T& a, const T& b) {
 	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
 tcT> bool ckmax(T& a, const T& b) {
 	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
-
 tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
 	sort(all(v));
 	v.erase(unique(all(v)), end(v));
@@ -154,34 +135,141 @@ int rng_int(int L, int R) { assert(L <= R);
 	return uniform_int_distribution<int>(L,R)(rng);  }
 ll rng_ll(ll L, ll R) { assert(L <= R);
 	return uniform_int_distribution<ll>(L,R)(rng);  }
-//? /Generator
 
-vl solve(vl &a,ll n,ll m) {
-	ll q=a.size();
-	set<ll> pos;
-	pos.insert(m);
-	FOR(i,0,q){
+#pragma once
+template<int MOD, int RT> struct mint {
+	static const int mod = MOD;
+	static constexpr mint rt() { return RT; } // primitive root for FFT
+	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+	mint():v(0) {}
+	mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD; }
+	bool operator==(const mint& o) const {
+		return v == o.v; }
+	friend bool operator!=(const mint& a, const mint& b) { 
+		return !(a == b); }
+	friend bool operator<(const mint& a, const mint& b) { 
+		return a.v < b.v; }
+	friend str ts(mint a) { return ts(a.v); }
+   
+	mint& operator+=(const mint& o) { 
+		if ((v += o.v) >= MOD) v -= MOD; 
+		return *this; }
+	mint& operator-=(const mint& o) { 
+		if ((v -= o.v) < 0) v += MOD; 
+		return *this; }
+	mint& operator*=(const mint& o) { 
+		v = int((ll)v*o.v%MOD); return *this; }
+	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1; assert(p >= 0);
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans; }
+	friend mint inv(const mint& a) { assert(a.v != 0); 
+		return pow(a,MOD-2); }
+		
+	mint operator-() const { return mint(-v); }
+	mint& operator++() { return *this += 1; }
+	mint& operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint& b) { return a += b; }
+	friend mint operator-(mint a, const mint& b) { return a -= b; }
+	friend mint operator*(mint a, const mint& b) { return a *= b; }
+	friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
 
+using mi = mint<MOD,5>; // 5 is primitive root for both common mods
+using vmi = V<mi>;
+using pmi = pair<mi,mi>;
+using vpmi = V<pmi>;
+
+V<vmi> scmb; // small combinations
+void genComb(int SZ) {
+	scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
+	FOR(i,1,SZ) F0R(j,i+1) 
+		scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
+}
+//const int TAG=3170;
+void add_factors(ll x,map<ll,ll> &toAdd){
+    int x2=x;
+    for(int i=2;i*i<=x;i++){
+        if(x2%i==0){
+            int cont=0;
+            while(x2%i==0){
+                cont++;
+                x2/=i;
+            }
+            if(cont>0){
+				toAdd[i]+=cont;
+            }
+        }
+    }
+    if(x2>1){
+		toAdd[x2]++;	
+    }
+}
+const int N=10000005;
+vb isPrime(N,1);
+vl primes;
+void init(){
+	FOR(i,2,N){
+		if(isPrime[i]){
+			for(ll j=2*i;j<N;j+=i){
+				isPrime[j]=0;
+			}
+		}
 	}
-	
+	FOR(i,2,N){
+		if(isPrime[i])
+			primes.pb(i);
+	}
+}
+map<ll,ll> getFactorsRapido(ll m){
+	map<ll,ll> decomp;
+	FOR(i,0,(ll)primes.size()){
+		ll prime=primes[i];
+		if(prime > m)break;
+		for(int j=prime;j<=m;j*=prime){
+			decomp[prime]+=m/j;
+		}
+	}
+	return decomp;
+}
+vl solve(vl &a,ll m) {
+	ll n=sz(a);
+	vector<map<ll,ll>> factors(n);
+	FOR(i,0,n){
+		add_factors(a[i],factors[i]); 
+	}
+	auto decomp=getFactorsRapido(m);
+	vl ans;
+	FOR(i,0,n){
+		mi res=mi(1);
+		
+		each(e,decomp){
+			factors[i][e.f]+=e.s;
+		}
+		each(e,factors[i]){
+			if(e.s+1>1)
+				res*=mi(e.s+1);
+		}
+		ans.pb((ll)res.v);
+	}
+	return ans;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-
     int t = 1;
-    cin >> t;
-
+	init();
     for(int idx = 0; idx < t; idx++) {
         RAYA;
         RAYA;
-		ll n,m,q;
-		cin>>n>>m>>q;
-		vl a(q);
+		ll n,m;
+		cin>>n>>m;
+		vl a(n);
 		each(e,a)cin>>e;
-
-        auto xd =solve(a,n,m);
-		eack(e,xd)cout<<e<<" ";
+		auto x = solve(a,m);
+		each(e,x)cout<<e<<" ";
 		cout<<"\n";
     }
     RAYA;
@@ -194,7 +282,6 @@ int main() {
         cerr << "\033[42m++++++++++++++++++++\033[0m";
     #endif
 }
-
 
 
 
