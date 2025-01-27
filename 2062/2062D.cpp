07f9f1a03f01da1d1a3fe2870fa0a844911d2f80
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2025-01-27 10:00:00 -0500

    Cf

--- 2062/2062D.cpp
+++ 2062/2062D.cpp
@@ -157,7 +157,7 @@ using vvi = V<vi>;
 using vvl = V<vl>;
 using vvb = V<vb>;
 
-const int MOD =998244353;
+const int MOD = 1e9 + 7;
 const int MX = (int)2e5 + 5;
 const ll BIG = 1e18;  //? not too close to LLONG_MAX
 const db PI = acos((db)-1);
@@ -173,96 +173,11 @@ ll rng_ll(ll L, ll R) { assert(L <= R);
 //? Template
 //? /Template
 
-/**
- * Description: modular arithmetic operations 
- * Source: 
-	* KACTL
-	* https://codeforces.com/blog/entry/63903
-	* https://codeforces.com/contest/1261/submission/65632855 (tourist)
-	* https://codeforces.com/contest/1264/submission/66344993 (ksun)
-	* also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp (ecnerwal)
- * Verification: 
-	* https://open.kattis.com/problems/modulararithmetic
- */
 
-#pragma once
-
-template<int MOD, int RT> struct mint {
-	static const int mod = MOD;
-	static constexpr mint rt() { return RT; } // primitive root for FFT
-	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
-	mint():v(0) {}
-	mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
-		if (v < 0) v += MOD; }
-	bool operator==(const mint& o) const {
-		return v == o.v; }
-	friend bool operator!=(const mint& a, const mint& b) { 
-		return !(a == b); }
-	friend bool operator<(const mint& a, const mint& b) { 
-		return a.v < b.v; }
-	friend str ts(mint a) { return ts(a.v); }
-   
-	mint& operator+=(const mint& o) { 
-		if ((v += o.v) >= MOD) v -= MOD; 
-		return *this; }
-	mint& operator-=(const mint& o) { 
-		if ((v -= o.v) < 0) v += MOD; 
-		return *this; }
-	mint& operator*=(const mint& o) { 
-		v = int((ll)v*o.v%MOD); return *this; }
-	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
-	friend mint pow(mint a, ll p) {
-		mint ans = 1; assert(p >= 0);
-		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
-		return ans; }
-	friend mint inv(const mint& a) { assert(a.v != 0); 
-		return pow(a,MOD-2); }
-		
-	mint operator-() const { return mint(-v); }
-	mint& operator++() { return *this += 1; }
-	mint& operator--() { return *this -= 1; }
-	friend mint operator+(mint a, const mint& b) { return a += b; }
-	friend mint operator-(mint a, const mint& b) { return a -= b; }
-	friend mint operator*(mint a, const mint& b) { return a *= b; }
-	friend mint operator/(mint a, const mint& b) { return a /= b; }
-};
-
-using mi = mint<MOD,5>; // 5 is primitive root for both common mods
-using vmi = V<mi>;
-using pmi = pair<mi,mi>;
-using vpmi = V<pmi>;
-
-V<vmi> scmb; // small combinations
-void genComb(int SZ) {
-	scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
-	FOR(i,1,SZ) F0R(j,i+1) 
-		scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
-}
 
 ll solve(vl &a) {
     ll n=a.size();
-	map<tuple<ll,ll,ll>,mi> dp;
-	if(a[0]==0){
-		dp[{0,0,0}]=mi(1);
-		dp[{0,0,1}]=mi(1);
-	}
-	FOR(i,1,n){
-		if(a[i]<=i){
-			if(a[i]>=1){
-				dp[{i,a[i],0}]+=dp.count({i-1,a[i]-1,1}) ? dp[{i-1,a[i]-1,1}] :mi(0);
-			}
-			if(a[i]==a[i-1]){
-				dp[{i,a[i],0}]+=dp.count({i-1,a[i-1],0}) ? dp[{i-1,a[i-1],0}] : mi(0);
-			}
-		}
-		dp[{i,a[i-1],1}]+=dp.count({i-1,a[i-1],0}) ? dp[{i-1,a[i-1],0}] : mi(0);
-	}
-	mi ans=dp.count({n-1,a[n-1],0}) ? dp[{n-1,a[n-1],0}] : mi(0);
-	if(n>=2){
-		ans+=dp.count({n-1,a[n-2],1}) ? dp[{n-1,a[n-2],1}] : mi(0);
-	}
-	return ans.v;
-	
+	return 0;
 }
 
 void setIn(str s) { freopen(s.c_str(), "r", stdin); }
